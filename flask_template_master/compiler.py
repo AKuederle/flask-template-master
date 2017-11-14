fcbd7580b6769be67975b23b7f64943230739644
Arne Kuederle <a.kuederle@gmail.com>
2017-11-14 22:15:07 +0100

    Implemented concept of compilers

---
+++ flask_template_master/compiler.py
@@ -0,0 +1,61 @@
+import subprocess
+
+import os
+from tempfile import gettempdir
+
+from io import BytesIO
+from flask import send_file
+
+
+class BaseCompiler:
+    def compile(self, template_name, document):
+        raise NotImplementedError()
+
+
+class SendFileCompiler(BaseCompiler):
+    """A simple compiler that tries to send the compiled template as a file instead of a simple request content."""
+    FILE_EXTENSION = None
+    FILE_PREFIX = None
+    FILE_POSTFIX = None
+
+    def __init__(self, file_extension=None, file_prefix=None, file_postfix=None):
+        self.FILE_EXTENSION = file_extension or self.FILE_EXTENSION
+        self.FILE_PREFIX = file_prefix or self.FILE_PREFIX
+        self.FILE_POSTFIX = file_postfix or self.FILE_POSTFIX
+
+    def _build_file_name(self, template_name):
+        file_name = ''
+        extension = '.' + self.FILE_EXTENSION if self.FILE_EXTENSION else None
+        for part in [self.FILE_PREFIX, template_name, self.FILE_POSTFIX, extension]:
+            if part:
+                file_name += part
+        return file_name
+
+    def _create_file(self, template_name, document):
+        buffer = BytesIO()
+        buffer.write(document.encode('utf-8'))
+        buffer.seek(0)
+        return buffer
+
+    def compile(self, template_name, document):
+        file = self._create_file(template_name, document)
+        return send_file(file, attachment_filename=self._build_file_name(template_name),
+                         as_attachment=True)
+
+
+class LatexCompiler(SendFileCompiler):
+    LATEX_COMMAND = 'pdflatex'
+    _OUT_DIR = gettempdir()
+    _TEMP_OUT_NAME = 'temp'
+    FILE_EXTENSION = 'pdf'
+
+    def _create_file(self, template_name, document):
+        tempfile = os.path.join(self._OUT_DIR, self._TEMP_OUT_NAME)
+        print(tempfile)
+        temp_tex = tempfile + '.tex'
+        with open(temp_tex, 'wb') as f:
+            f.write(document.encode('utf-8'))
+        proc = subprocess.Popen([*self.LATEX_COMMAND.split(' '), temp_tex], cwd=self._OUT_DIR)
+        proc.wait()
+        print(self.FILE_EXTENSION)
+        return tempfile + '.' + self.FILE_EXTENSION
--- flask_template_master/views.py
+++ flask_template_master/views.py
@@ -8,17 +8,20 @@ class BaseTemplateView(Resource):
     """Base View To Provide Templates.
 
     Attributes:
-        GLOBALS: A dictionary of variables that is shared by all templates
+        GLOBAL_PROVIDER: A Class instance that handels global variables
         ENVIRONMENT: A jinja2 Environment providing a template loader that supports list_templates
+        COMPILER: A class instance that handles optionally compiling documents after the templating step
     """
     ENVIRONMENT = None
     GLOBAL_PROVIDER = None
+    COMPILER = None
 
     def get(self, template_name=None):
         """Return list of templates or details of a single template"""
         if not template_name:
             match_filter = request.args.get('filter', default='*', type=str)
-            return {'templates': self.ENVIRONMENT.list_templates(filter_func=lambda x: fnmatch.fnmatch(x, match_filter))}
+            return {
+                'templates': self.ENVIRONMENT.list_templates(filter_func=lambda x: fnmatch.fnmatch(x, match_filter))}
         source = str(self.ENVIRONMENT.loader.get_source(self.ENVIRONMENT, template_name)[0])
         return_dict = {
             'template_name': template_name,
@@ -37,7 +40,10 @@ class BaseTemplateView(Resource):
             vars = {}
         if self.GLOBAL_PROVIDER:
             vars = {**self.GLOBAL_PROVIDER.get_globals(), **vars}
-        return self.ENVIRONMENT.get_template(template_name).render(**vars)
+        document = self.ENVIRONMENT.get_template(template_name).render(**vars)
+        if self.COMPILER:
+            return self.COMPILER.compile(template_name, document)
+        return document
 
     @classmethod
     def add_as_resource(cls, api, base_route, argument=None):
@@ -45,11 +51,14 @@ class BaseTemplateView(Resource):
         api.add_resource(cls, base_route, base_route + argument)
 
 
-def create_template_endpoint(api, base_route, argument=None, environment=None, global_provider=None):
+def create_template_endpoint(api, base_route, argument=None, environment=None, global_provider=None, compiler=None,
+                             view_class=BaseTemplateView):
     class_dict = {
         'ENVIRONMENT': environment,
-        'GLOBAL_PROVIDER': global_provider
+        'GLOBAL_PROVIDER': global_provider,
+        'COMPILER': compiler
     }
-    class_view = type(''.join(e for e in base_route if e.isalnum()), (BaseTemplateView, ), class_dict)
+    # Dynamically create a new Class (Not a class Instance of the BaseTemplateView
+    class_view = type(''.join(e for e in base_route if e.isalnum()), (view_class,), class_dict)
     class_view.add_as_resource(api, base_route, argument)
     return class_view
