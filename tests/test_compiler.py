e81f2dba6fdef8138c0a017e5350bc5808e9712d
Arne Kuederle <a.kuederle@gmail.com>
2017-11-21 21:18:59 +0100

    test for filename and latex functional test

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,7 +1,7 @@
 import pytest
 from pytest import raises
 
-from flask_template_master.compiler import BaseCompiler, SendFileCompiler
+from flask_template_master.compiler import BaseCompiler, SendFileCompiler, LatexCompiler
 
 
 def test_base_compiler_raises_error():
@@ -15,6 +15,21 @@ def file_compiler():
     return SendFileCompiler()
 
 
+@pytest.fixture(scope='module')
+def latex_compiler():
+    return LatexCompiler()
+
+
+@pytest.fixture(scope='module')
+def minimal_tex():
+    return r"""
+    \documentclass{article} 
+    \begin{document}
+    First document. This is a simple example, with no 
+    extra parameters or packages included.
+    \end{document}"""
+
+
 def test_file_compiler_attachment_header(file_compiler, app):
     response = file_compiler.compile('test', 'test_text')
     assert 'attachment' in response.headers['Content-Disposition']
@@ -24,3 +39,14 @@ def test_file_compiler_sends_created_name(file_compiler, app, monkeypatch):
     monkeypatch.setattr(file_compiler, '_build_file_name', lambda x: 'mock_called')
     response = file_compiler.compile('test', 'test_text')
     assert 'filename=mock_called' in response.headers['Content-Disposition']
+
+
+def test_build_filename():
+    compiler = SendFileCompiler(file_extension='ext', file_prefix='pre', file_postfix='post')
+    assert compiler._build_file_name('file') == 'prefilepost.ext'
+
+
+def test_latex_compiler_works(latex_compiler, app, minimal_tex):
+    response = latex_compiler.compile('test', minimal_tex)
+    assert 'application/pdf' in response.headers['Content-Type']
+    assert response.response.file.raw.read().startswith(b'%PDF')
